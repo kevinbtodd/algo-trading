b319a0d252d5f5cad9b210e67096d7680ce859f1
Kevin <kbtodd00@gmail.com>
2018-01-12 15:25:52 +0000

    added linear interpolation

--- analysis.py
+++ analysis.py
@@ -1,35 +1,69 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import sqlite3
 
-period = 5000
+# period for the moving average
+period = 20000
 
-# connect to sqlite database 'tickdata.db'
+# connect to SQL database 'tickdata.db'
 conn = sqlite3.connect('ticks.db')
 
-# cex_data = pd.read_sql_query("""SELECT id, bid, ask FROM cex WHERE id > 268307 ORDER BY id;""", conn)
+# load data from SQL database
 bfx_data = pd.read_sql_query("""SELECT * FROM bitfinex;""", conn)
 krk_data = pd.read_sql_query("""SELECT * FROM kraken;""", conn)
 
+# dissconnect from database
 conn.commit()
 conn.close()
 
-long_bfx = -bfx_data['ask'] + krk_data['bid']
-# short_bfx = bfx_data['bid'] - cex_data['ask']
+# calculate midpoint between bid and ask for each asset
+bfx_data['mid'] = (bfx_data['bid'] + bfx_data['ask']) / 2
+krk_data['mid'] = (krk_data['bid'] + krk_data['ask']) / 2
 
-ma = pd.rolling_mean(long_bfx, period)
-moving_sd = pd.rolling_std(long_bfx, period) 
+# create a timestamp array with 1 second intervals
+total_rows = np.shape(bfx_data['timestamp'])[0]
+timestamp_min = int(bfx_data['timestamp'][0])
+timestamp_max = int(bfx_data['timestamp'][total_rows - 1])
+timestamps = pd.DataFrame({'timestamp':range(timestamp_min , timestamp_max, 1)})
 
-moving_z = (long_bfx - ma) * (1/moving_sd)
+# create complete arrays
+bfx_complete = pd.merge(timestamps, bfx_data,
+                        how='left', left_on='timestamp', right_on='timestamp'
+                        ).drop(['id', 'bid', 'ask'], axis=1)
+krk_complete = pd.merge(timestamps, krk_data,
+                        how='left', left_on='timestamp', right_on='timestamp'
+                        ).drop(['id', 'bid', 'ask'], axis=1)
 
+# interpolate missing values with linear interpolation
+bfx_complete = bfx_complete.interpolate(method='linear')
+krk_complete = krk_complete.interpolate(method='linear')
+
+# calculate spread
+spread = krk_complete['mid'] - bfx_complete['mid']
+
+# plot spread
+plt.plot(spread)
+
+# calculate moving average and standard deviation
+moving_average = spread.rolling(window=period, center=False).mean()
+moving_sd = spread.rolling(window=period, center=False).std()
+
+# standardise spread
+moving_z = (spread - moving_average) * (1/moving_sd)
+
+# plot standardised spread
 plt.plot(moving_z)
 
+# plot spread and moving average
 ax1 = plt.subplot(211)
-plt.plot(long_bfx)
-plt.plot(ma)
+plt.plot(spread)
+plt.plot(moving_average)
 
+# plot standardised spread and standard deviation bars
 ax2 = plt.subplot(212, sharex=ax1)
 plt.plot(moving_z)
 plt.axhline(0, color='black')
 plt.axhline(4.0, color='red', linestyle='--')
 plt.axhline(-4.0, color='green', linestyle='--')
+
